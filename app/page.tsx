e8f8114698dea83ed637faadbdd6be8f275a06e7
austrange-ali <austrange.ali@gmail.com>
2025-11-15 15:59:06 +0530

    removed demo box

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import { HoverBorderGradientDemo } from "@/components/button";
 import { HeroParallax } from "@/components/ui/hero-parallax";
-import AnimatedBox from "@/components/animatedbox";
 
 
 
@@ -76,7 +75,6 @@ export default function Home() {
     <div className="w-full overflow-x-hidden">
       <section className="w-full overflow-x-hidden">
         <HeroParallax products={products} />
-        <AnimatedBox  />
       </section>
     </div>
   );
